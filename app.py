1250eccc3af9f46ac7ddee4a28a9c9e91b13181c
Ryan <zhaoryan20@gmail.com>
2025-04-12 13:44:31 -0700

    finished the chat messaging

--- app.py
+++ app.py
@@ -2,9 +2,13 @@ import streamlit as st
 from dotenv import load_dotenv
 from PyPDF2 import PdfFileReader
 from langchain.text_splitter import CharacterTextSplitter
-from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import OpenAIEmbeddings, HuggingFaceInstructEmbeddings
 from langchain.vectorstores import FAISS
 from langchain_deepseek import ChatDeepSeek
+from langchain.chat_models import ChatOpenAI
+from langchain.memory import ConversationBufferMemory
+from langchain.chains import conversational_retrieval
+from htmlTemplate import css, bot_template, user_template
 
 def get_pdf_text(pdf_docs):
     text = ""
@@ -25,17 +29,49 @@ def get_text_chunks(text):
     return chunks
 
 def get_vectorstore(text_chunks):
-    embeddings = OpenAIEmbeddings()
+    # embeddings = OpenAIEmbeddings()
+    embeddings = HuggingFaceInstructEmbeddings(model_name="deepseek-ai/DeepSeek-V3-0324")
     vectorstore = FAISS.from_texts(texts=text_chunks, embedding=embeddings)
     return vectorstore
+
+def get_conversation_chain(vectorstore):
+    llm = ChatDeepSeek()
+    memory = ConversationBufferMemory(memory_key='chat_history', return_messages=True)
+    conversation_chain = conversational_retrieval(
+        llm=llm,
+        retriever=vectorstore.as_retriever(),
+        memory=memory
+    )
+    return conversation_chain
+
+def handle_userinput(user_question):
+    response = st.session_state.conversation({'question': user_question})
+    st.session_state.chat_history = response('chat_history')
+
+    for i, message in enumerate(st.session_state.chat_history):
+        if i % 2 == 0:
+            st.write(user_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
+        else:
+            st.write(bot_template.replace("{{MSG}}", message.content), unsafe_allow_html=True)
 
 
 def main():
     load_dotenv()
     st.set_page_config(page_title="PDF Chatbot", page_icon=":books")
 
+    st.write(css, unsafe_allow_html=True)
+
+    # Check if the conversation is in session state of application
+    if "conversation" not in st.session_state:
+        st.session_state.conversation = None
+    if "chat_history" not in st.session_state:
+        st.session_state.chat_history = None
+
     st.header("Chat with multiple PDFs")
-    st.text_input("Ask about your documents: ")
+    user_question = st.text_input("Ask about your documents: ")
+
+    if user_question:
+        handle_userinput(user_question)
 
     with st.sidebar:
         st.subheader("Your PDFs")
@@ -51,6 +87,8 @@ def main():
                 # Create the vector store with the embeddings
                 vectorstore = get_vectorstore(text_chunks)
 
+                # create conversation chain
+                st.session_state.conversation = get_conversation_chain(vectorstore)
 
 if __name__ == '__main__':
     main()
